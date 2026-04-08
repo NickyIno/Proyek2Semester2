c4f74323d88bb9e401e04cf823500d86354d3da8
Nicky.Ino <reffalino373@gmail.com>
2026-04-08 11:58:43 +0700

    Change delete query to remove user from users table

--- aksi/hapusAkun.php
+++ aksi/hapusAkun.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
 include 'koneksi.php';
 session_start();
 
@@ -7,7 +8,7 @@ if (!isset($_SESSION['username'])) { exit; }
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     
-    $query = "DELETE FROM master_kas WHERE id = '$id'";
+    $query = "DELETE FROM users WHERE id = '$id'";
     
     if (mysqli_query($koneksi, $query)) {
         header("Location: ../user/dashboard.php?pesan=hapus_berhasil");
