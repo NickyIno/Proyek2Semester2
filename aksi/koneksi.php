1c1d8bd0afd7db2d5a1c8d8bfd3c0f8b2dfe2e03
Nicky.Ino <reffalino373@gmail.com>
2026-05-01 19:57:41 +0700

    Change database name from 'warung_tahu_kupat' to 'Kas'

--- aksi/koneksi.php
+++ aksi/koneksi.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "warung_tahu_kupat";
+$database = "Kas";
 
 $koneksi = mysqli_connect($hostname, $username, $password, $database);
 
