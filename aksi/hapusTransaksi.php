94eb9596118fc977894533f45dfd4689a5ff0afe
Nicky.Ino <reffalino373@gmail.com>
2026-05-07 12:23:02 +0700

    Update hapusTransaksi.php

--- aksi/hapusTransaksi.php
+++ aksi/hapusTransaksi.php
@@ -8,11 +8,9 @@ if (isset($_GET['id_transaksi']) && isset($_GET['back'])) {
     $id_trans  = $_GET['id_transaksi'];
     $id_master = $_GET['back']; 
     
-    // SESUAIKAN: Apakah nama kolom di tabel database 'id' atau 'id_transaksi'?
     $query = "DELETE FROM transaksi_kas WHERE id = '$id_trans'";
     
     if (mysqli_query($koneksi, $query)) {
-        // Redirect kembali ke buku kas berdasarkan ID master
         header("Location: ../user/bukuKas.php?id=" . $id_master);
         exit();
     } else {
